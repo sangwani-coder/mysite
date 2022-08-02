eb1fda1d445010d24621bdb3f55619ed50263a82
zyambo <mightypz2@gmail.com>
2022-08-02 22:40:01 +0200

    add contact me page

--- zyambo/static/js/script.js
+++ zyambo/static/js/script.js
@@ -1,21 +1,24 @@
 // type bio
 let i = 0;
-const bio = 'My name is Peter Zyambo a software engineering student at ALX, specializing in backend engineering.\
-I love solving problems and buid fullstack web and desktop applications.\
-In my spare time I study about Human Computer Interaction (HCI) and Database Design.\
-My motto in life is to AIM HIGH.';
+const bio = "I'm a software engineering student specializing in backend engineering.\
+I love to create fullstack web and desktop applications.\
+In my spare time I study about Human Computer Interaction, Physics and History.\
+My life motto is AIM HIGH.";
 
 const lang = 'Python, js, C, MySQL';
 const speed = 55;
 
-function typeWriter() {
-        if ( i < bio.length) {
-            if ( bio.charAt(i-1) === '.'){
-                document.getElementById("bio").innerHTML += "<br>";    
-            }
-            document.getElementById("bio").innerHTML += bio.charAt(i);
-            i++;
-            setTimeout(typeWriter , speed);
+
+function typeWriter() { 
+    if ( i < bio.length) {
+        if ( bio.charAt(i-1) === '.'){
+            document.getElementById("bio").innerHTML += "<br>";    
         }
+        document.getElementById("bio").innerHTML += bio.charAt(i);
+        i++;
+        setTimeout(typeWriter , speed);
+    }
 }
-    
+function modify() {
+    document.getElementById('hide').classList.add('ready');
+}
