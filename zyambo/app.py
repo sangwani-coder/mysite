c0d5c5440ec56499e4966d9cce3a2d3b17e8a2fa
zyambo <mightypz@gmail.com>
2022-08-03 19:47:38 +0000

    comment if name

--- zyambo/app.py
+++ zyambo/app.py
@@ -17,7 +17,7 @@ def unauthorized(error) -> str:
     return render_template('error.html', error=error), 401
 
 
-if __name__ == "__main__":
-    app.jinja_env.auto_reload = True
-    app.config['TEMPLATES_AUTO_RELOAD'] = True
-    app.run(debug=True, host='0.0.0.0')
+# if __name__ == "__main__":
+#     app.jinja_env.auto_reload = True
+#     app.config['TEMPLATES_AUTO_RELOAD'] = True
+#     app.run(debug=True, host='0.0.0.0')
